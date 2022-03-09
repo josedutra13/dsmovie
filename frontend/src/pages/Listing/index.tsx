22a179a9eb8fd28d01fe7abd6edc806332f393e6
josé.dutra <63750132+josedutra13@users.noreply.github.com>
2022-03-09 00:21:41 -0300

    MovieCard

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,14 @@
+import MovieCard from "components/MovieCard";
+import MovieScore from "components/MovieScore";
 import Pagination from "components/Pagination";
 
 function Listing(){
     return(
+        <>
         <Pagination/>
+        <MovieCard/>
+        </>
+        
     )
 }
 
